725c895dfdffbe76811c4e0b95f14744d547e281
Racheal <3068566k@student.gla.ac.uk>
2025-03-16 13:34:07 +0000

    Course class

---
+++ Course.java
@@ -0,0 +1,45 @@
+public class Course {
+    private int id;
+    private String name;
+    private int trainingSessions;
+
+    public Course(int id, String name, int trainingSessions) {
+        this.id = id;
+        this.name = name;
+        this.trainingSessions = trainingSessions;
+    }
+
+
+    public static Course createCourse(int id, String name, int trainingSessions) {
+        return new Course(id, name, trainingSessions);
+    }
+
+    public void updateCourse(String name, int trainingSessions) {
+        this.name = name;
+        this.trainingSessions = trainingSessions;
+        System.out.println("Course updated: ID " + id + ", Name: " + name + ", Training Sessions: " + trainingSessions);
+    }
+
+
+    public void deleteCourse() {
+        System.out.println("Course deleted: ID " + id + ", Name: " + name);
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getTrainingSessions() {
+        return trainingSessions;
+    }
+
+    @Override
+    public String toString() {
+        return "Course: ID " + id + ", Name: " + name + ", Training Sessions: " + trainingSessions;
+    }
+}
